a867ec48a796161786ba98a3ccc9b1e33048bf27
vutii <giftmuuo.com@gmail.com>
2026-04-14 14:18:20 +0300

    update

--- app/src/main/java/com/example/sokogarden/Signup.kt
+++ app/src/main/java/com/example/sokogarden/Signup.kt
@@ -1,10 +1,15 @@
 package com.example.sokogarden
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,36 @@ class Signup : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+//        find all views by use of their ids
+        val username = findViewById<EditText>(R.id.username)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val phone = findViewById<EditText>(R.id.phone)
+        val signupButton = findViewById<Button>(R.id.signupBtn)
+        val signinTextView = findViewById<TextView>(R.id.signintxt)
+
+        //below when a person clicks on the TextView, he/she is navigated to the singin page
+        signinTextView.setOnClickListener {
+            val intent = Intent(applicationContext, Signin::class.java)
+            startActivity(intent)
+        }
+//        on click of the signup button we want to register a person
+        signupButton.setOnClickListener {
+//            specify the api endpoint
+            val api = "https://kbenkamotho.alwaysdata.net/api/signup"
+//            create a requestparams -> soasto hold data
+            val data = RequestParams()
+//            add/append username,email,password and phone on the data
+            data.put("username", username.text.toString().trim())
+            data.put("email", email.text.toString().trim())
+            data.put("password", password.text.toString().trim())
+            data.put("phone", phone.text.toString().trim())
+//            import api helper
+            val helper = ApiHelper(applicationContext)
+//            inside the helper access the functions post
+            helper.post(api, data)
         }
     }
 }
